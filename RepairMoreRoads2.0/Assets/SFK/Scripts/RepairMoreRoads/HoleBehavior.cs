d5f97f2ca48624b84dfb09c14d2f4331f871d6dd
QIYE7777 <931938629@qq.com>
2023-03-14 23:40:39 +0800

    2023.3.14
    1.people up and down
    2.add a sound effect
    3add a bgm

--- RepairMoreRoads2.0/Assets/SFK/Scripts/RepairMoreRoads/HoleBehavior.cs
+++ RepairMoreRoads2.0/Assets/SFK/Scripts/RepairMoreRoads/HoleBehavior.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 public class HoleBehavior : MonoBehaviour
 {
     public Transform fillHoleTrans;
+    public AudioSource closeDoor;
 
+    private void Awake()
+    {
+        closeDoor = GetComponent<AudioSource>();
+    }
     public void FillHole()
     {
         fillHoleTrans.DOScaleZ(2, 1).SetEase(Ease.InCubic) ;
+        
+        closeDoor.Play();
     }
 
     public void ShootBall()
---
+++ RepairMoreRoads2.0/Assets/SFK/Scripts/RepairMoreRoads/PeopleMovement.cs
@@ -0,0 +1,17 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class PeopleMovement : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
