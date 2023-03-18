50beb8e75c26696f6ce867a1779d32b80b38f6bd
QIYE7777 <931938629@qq.com>
2023-03-18 17:05:22 +0800

    2023.3.17
    people move

--- RepairMoreRoads2.0/Assets/SFK/Scripts/RepairMoreRoads/PeopleMovement.cs
+++ RepairMoreRoads2.0/Assets/SFK/Scripts/RepairMoreRoads/PeopleMovement.cs
@@ -3,15 +3,27 @@ using UnityEngine;
 
 public class PeopleMovement : MonoBehaviour
 {
+    public CharacterController cc;
+    public Vector3 movement { get; private set; }
+    public float speed;
+    public float h;
+    public float v;
 
-    void Start()
+    private void Awake()
     {
-        
+        cc = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-    
+        Move(h,v );
+    }
+
+    void Move(float h, float v)
+    {
+        movement = new Vector3(h, 0f, v);
+        //movement = movement.normalized * speed * Time.deltaTime;
+        var realSpeed = speed;
+        cc.SimpleMove(movement.normalized * realSpeed);
     }
 }
